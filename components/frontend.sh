5fac3d64d2c93711a088e2567393125d97429adb
= <=>
2022-10-24 23:14:27 +0530

    hh

--- components/frontend.sh
+++ components/frontend.sh
@@ -7,18 +7,18 @@ if [ $USERID -ne 0 ] ; then
     exit 1
 fi
 
-yum install nginx -y  
+yum install nginx -y  &>> /tmp/frontend.log
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 
-rm -rf /usr/share/nginx/html/*  
+rm -rf /usr/share/nginx/html/*  &>> /tmp/frontend.log
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
-rm -rf frontend-main README.md
+rm -rf frontend-main README.md &>> /tmp/frontend.log
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx 
-systemctl start nginx  
+systemctl enable nginx &>> /tmp/frontend.log
+systemctl start nginx  &>> /tmp/frontend.log
 
