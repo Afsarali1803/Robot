54cd6fae0cc3c22a16395d456f5e7eca1e682e01
= <=>
2022-10-24 21:27:08 +0530

    frontend

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-echo " This is frontend component"
+yum install nginx -y
 
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+
+rm -rf /usr/share/nginx/html
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl enable nginx
+systemctl start nginx
 
