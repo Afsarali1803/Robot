bd1c7a1fc854adcbc36b07ee60bcb24155b1b112
= <=>
2022-10-26 22:06:57 +0530

    mod frontend

--- components/frontend.sh
+++ components/frontend.sh
@@ -2,8 +2,7 @@
 
 USERID=$(id -u)
 COMPONENT=frontend
-echo &COMPONENT
-LOGFILE=/tmp/&COMPONENT.log
+LOGFILE=/tmp/$COMPONENT.log
 
 if [ $USERID -ne 0 ] ; then
     echo -e "Run as a root user"
@@ -19,24 +18,24 @@ else
 fi
 
 echo -n " Installing the nginx:"
-yum install nginx -y  &>> &LOGFILE
+yum install nginx -y  &>> $LOGFILE
 stat $?
 
 echo -n " Downloading the component:"
 curl -s -L -o /tmp/&COMPONENT.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-rm -rf /usr/share/nginx/html/*  &>> &LOGFILE
+rm -rf /usr/share/nginx/html/*  &>> $LOGFILE
 cd /usr/share/nginx/html
-unzip /tmp/&COMPONENT.zip &>> &LOGFILE
+unzip /tmp/&COMPONENT.zip &>> $LOGFILE
 stat $?
 
 mv &COMPONENT-main/* .
 mv static/* .
 echo -n "Perform cleanup:"
-rm -rf &COMPONENT-main README.md &>> &LOGFILE
+rm -rf &COMPONENT-main README.md &>> $LOGFILE
 stat $?
 
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx &>> &LOGFILE
-systemctl start nginx  &>> &LOGFILE
+systemctl enable nginx &>> $LOGFILE
+systemctl start nginx  &>> $LOGFILE
 
