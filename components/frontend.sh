9edded477ee52c02093432e2045f8dd3d54d2a21
= <=>
2022-10-26 22:02:38 +0530

    mod frontend

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 USERID=$(id -u)
+COMPONENT=frontend
+LOGFILE=/tmp/&COMPONENT.log
 
 if [ $USERID -ne 0 ] ; then
     echo -e "Run as a root user"
     exit 1
 fi
 
-stat()
+stat(){
 
 if [ $1 -eq 0 ]; then
     echo -e "Success" 
@@ -15,26 +17,25 @@ else
     echo -e "Failure"
 fi
 
-
 echo -n " Installing the nginx:"
-yum install nginx -y  &>> /tmp/frontend.log
+yum install nginx -y  &>> &LOGFILE
 stat $?
 
 echo -n " Downloading the component:"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-rm -rf /usr/share/nginx/html/*  &>> /tmp/frontend.log
+curl -s -L -o /tmp/&COMPONENT.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+rm -rf /usr/share/nginx/html/*  &>> &LOGFILE
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
+unzip /tmp/&COMPONENT.zip &>> &LOGFILE
 stat $?
 
-mv frontend-main/* .
+mv &COMPONENT-main/* .
 mv static/* .
 echo -n "Perform cleanup:"
-rm -rf frontend-main README.md &>> /tmp/frontend.log
+rm -rf &COMPONENT-main README.md &>> &LOGFILE
 stat $?
 
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx &>> /tmp/frontend.log
-systemctl start nginx  &>> /tmp/frontend.log
+systemctl enable nginx &>> &LOGFILE
+systemctl start nginx  &>> &LOGFILE
 
