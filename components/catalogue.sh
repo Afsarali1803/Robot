cd9ef00368222f1b011324c8a6105ecbeb948e64
= <=>
2022-10-27 20:54:35 +0530

    mod catalogue

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo " This is catalogue component"
+COMPONENT=catalogue
+source components/common.sh 
+LOGFILE=/tmp/$COMPONENT.log
+APPUSER=roboshop
+
+echo -n "Configuring Node:"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOGFILE
+stat $?
+
+echo -n "Install the node js Application"
+yum install nodejs -y
+stat $?
+
+echo -n "Creating user"
+USERADD $APPUSER
+stat $?
+
+echo -n "Download the $COMPONENT:"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+stat $?
+
+
+
+
+
