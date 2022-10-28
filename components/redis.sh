6616810441f0d68156d2dbe683a0385c5a182b29
= <=>
2022-10-28 23:33:01 +0530

    mod redis

--- components/redis.sh
+++ components/redis.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo " This is redis component"
+USERID=$(id -u)
+COMPONENT=frontend
+LOGFILE=/tmp/$COMPONENT.log
+source components/common.sh 
+
+echo -n " Installing the nginx:"
+yum install redis-6.2.7 -y  &>> $LOGFILE
+stat $?
+
+echo -n " Downloading the component:"
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+stat $?
