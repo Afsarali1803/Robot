7dd591c3a4a978d110fd6b8990c632cba8af4b4e
= <=>
2022-10-28 23:34:16 +0530

    mod redis

--- components/redis.sh
+++ components/redis.sh
@@ -5,10 +5,13 @@ COMPONENT=frontend
 LOGFILE=/tmp/$COMPONENT.log
 source components/common.sh 
 
-echo -n " Installing the nginx:"
-yum install redis-6.2.7 -y  &>> $LOGFILE
-stat $?
+
 
 echo -n " Downloading the component:"
 curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+stat $?
+
+
+echo -n " Installing the nginx:"
+yum install redis-6.2.7 -y  &>> $LOGFILE
 stat $?
