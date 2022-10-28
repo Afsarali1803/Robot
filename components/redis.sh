590b7b8ffa9ded5a43bc088efc577f9f56b3b2ae
= <=>
2022-10-28 23:40:48 +0530

    mod redis

--- components/redis.sh
+++ components/redis.sh
@@ -15,3 +15,13 @@ stat $?
 echo -n " Installing the nginx:"
 yum install redis-6.2.7 -y  &>> $LOGFILE
 stat $?
+
+echo -n "whitelisting redis:"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+stat $
+
+echo -n "Starting service"
+systemctl daemon-reload
+systemctl start redis
+systemctl status redis -l &>> LOGFILE
+echo $?
