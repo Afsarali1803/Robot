9d05d1809253f6e51efb1982f5435d4fc194880c
= <=>
2022-11-03 19:01:33 +0530

    mod sql

--- components/mysql.sh
+++ components/mysql.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo " This is mysql component"
+USERID=$(id -u)
+COMPONENT=cart
+source components/common.sh 
+LOGFILE=/tmp/$COMPONENT.log
+APPUSER=roboshop
+
+echo -n "Configuring Node:"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo  &>> $LOGFILE
+stat $?
+
+echo -n "Installing $COMPONENT:"
+yum install mysql-community-server -y &>> $LOGFILE 
+stat $? 
+
+echo -n "Starting $COMPONENT service: "
+systemctl enable mysqld && systemctl start mysqld
+stat $?
