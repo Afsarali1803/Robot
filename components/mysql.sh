8b990c89c9a394771af98a08a0cefbca6944ce16
= <=>
2022-11-03 19:30:04 +0530

    mod sql

--- components/mysql.sh
+++ components/mysql.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-USERID=$(id -u)
-COMPONENT=mysql
-source components/common.sh 
-LOGFILE=/tmp/$COMPONENT.log
-APPUSER=roboshop
+#!/bin/bash 
 
-echo -n "Configuring Node:"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo  &>> $LOGFILE
-stat $?
+COMPONENT=mysql
+
+source components/common.sh
+
+read -p 'Enter MySQL Password you wish to configure:' MYSQL_PWD
+
+echo -n "Configuring the $COMPONENT Repo:"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE 
+stat $? 
 
 echo -n "Installing $COMPONENT:"
 yum install mysql-community-server -y &>> $LOGFILE 
@@ -20,6 +22,15 @@ stat $?
 
 echo -n "Changing the default password:"
 DEF_ROOT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' ' '{print $NF}')
+
+# I only want to change the default password only for the first time.
+# How do I come to know whether the password is changed or not.
+echo show databases | mysql -uroot -p${MYSQL_PWD} &>> $LOGFILE 
+if [ $? -ne 0 ] ; then 
+    echo -n "Reset Root Password"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PWD}';" | mysql  --connect-expired-password  -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE 
+    stat $? 
+fi 
 
 echo show plugins | mysql -uroot -p${MYSQL_PWD} | grep validate_password; &>> $LOGFILE 
 if [ $? -eq 0 ] ; then 
